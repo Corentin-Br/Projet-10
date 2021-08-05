43be9fb4cd74cea2c2c5762b8600f444fe10aa08
Corentin BRAVO <corentinbravo@gmail.com>
2021-08-05 14:27:46 +0200

    MyUserSerializer data now asks two passwords. It validates that they are the same, and it uses the MyUserManager to create users. reverse relationships are read_only.

--- Softdesk/softdeskAPI/serializers.py
+++ Softdesk/softdeskAPI/serializers.py
@@ -3,17 +3,34 @@ from .models import MyUser, Issue, Project, Contributor, Comment
 
 
 class MyUserSerializer(serializers.ModelSerializer):
-    issues_written = serializers.PrimaryKeyRelatedField(many=True, queryset=Issue.objects.all())
-    comments_written = serializers.PrimaryKeyRelatedField(many=True, queryset=Comment.objects.all())
-    contributors = serializers.PrimaryKeyRelatedField(many=True, queryset=Contributor.objects.all())
+    # issues_written = serializers.PrimaryKeyRelatedField(many=True, queryset=Issue.objects.all())
+    # comments_written = serializers.PrimaryKeyRelatedField(many=True, queryset=Comment.objects.all())
+    # contributors = serializers.PrimaryKeyRelatedField(many=True, queryset=Contributor.objects.all())
+    issues_written = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments_written = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    contributors = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    password1 = serializers.CharField(label='Password', max_length=50, write_only=True)
+    password2 = serializers.CharField(label='Password confirmation', max_length=50, write_only=True)
 
     class Meta:
         model = MyUser
-        fields = ['email', 'first_name', 'last_name', 'issues_written', 'comments_written', 'contributors']
+        fields = ['email', 'first_name', 'last_name', 'password1', 'password2', 'issues_written', 'comments_written', 'contributors']
+
+    def validate(self, data):
+        if data['password1'] == data['password2']:
+            return data
+        raise serializers.ValidationError("passwords must be identical")
+
+    def create(self, validated_data):
+        validated_data["password"] = validated_data["password1"]
+        del(validated_data['password1'])
+        del (validated_data['password2'])
+        return MyUser.objects.create_user(**validated_data)
 
 
 class IssueSerializer(serializers.ModelSerializer):
-    comments = serializers.PrimaryKeyRelatedField(many=True, queryset=Comment.objects.all())
+    # comments = serializers.PrimaryKeyRelatedField(many=True, queryset=Comment.objects.all())
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Issue
@@ -29,8 +46,10 @@ class ContributorSerializer(serializers.ModelSerializer):
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    contributors = serializers.PrimaryKeyRelatedField(many=True, queryset=Contributor.objects.all())
-    issues = serializers.PrimaryKeyRelatedField(many=True, queryset=Issue.objects.all())
+    # contributors = serializers.PrimaryKeyRelatedField(many=True, queryset=Contributor.objects.all())
+    # issues = serializers.PrimaryKeyRelatedField(many=True, queryset=Issue.objects.all())
+    contributors = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    issues = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Project
