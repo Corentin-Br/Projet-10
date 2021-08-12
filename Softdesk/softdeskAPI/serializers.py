fac4fd9decede89a81834555472acec79e02388f
Corentin BRAVO <corentinbravo@gmail.com>
2021-08-12 09:01:15 +0200

    Removed password1/password2 in the User serializer (it's the front-end job)

--- Softdesk/softdeskAPI/serializers.py
+++ Softdesk/softdeskAPI/serializers.py
@@ -6,58 +6,50 @@ class MyUserSerializer(serializers.ModelSerializer):
     # issues_written = serializers.PrimaryKeyRelatedField(many=True, queryset=Issue.objects.all())
     # comments_written = serializers.PrimaryKeyRelatedField(many=True, queryset=Comment.objects.all())
     # contributors = serializers.PrimaryKeyRelatedField(many=True, queryset=Contributor.objects.all())
-    issues_written = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    comments_written = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    contributors = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    password1 = serializers.CharField(label='Password', max_length=50, write_only=True)
-    password2 = serializers.CharField(label='Password confirmation', max_length=50, write_only=True)
+
+    issues_written_id = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments_written_id = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    contributors_id = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = MyUser
-        fields = ['email', 'first_name', 'last_name', 'password1', 'password2', 'issues_written', 'comments_written', 'contributors']
-
-    def validate(self, data):
-        if data['password1'] == data['password2']:
-            return data
-        raise serializers.ValidationError("passwords must be identical")
+        fields = ['email', 'first_name', 'last_name', 'password', 'issues_written_id', 'comments_written_id',
+                  'contributors_id']
 
     def create(self, validated_data):
-        validated_data["password"] = validated_data["password1"]
-        del(validated_data['password1'])
-        del (validated_data['password2'])
         return MyUser.objects.create_user(**validated_data)
 
 
 class IssueSerializer(serializers.ModelSerializer):
     # comments = serializers.PrimaryKeyRelatedField(many=True, queryset=Comment.objects.all())
-    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments_id = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Issue
-        fields = ['title', 'desc', 'tag', 'priority', 'status', 'project', 'author', 'assignee', 'created_time',
-                  'comments']
+        fields = ['title', 'desc', 'tag', 'priority', 'status', 'project_id', 'author_id', 'assignee_id',
+                  'created_time', 'comments_id']
 
 
 class ContributorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Contributor
-        fields = ['user', 'project', 'permission', 'role']
+        fields = ['user_id', 'project_id', 'permission', 'role']
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     # contributors = serializers.PrimaryKeyRelatedField(many=True, queryset=Contributor.objects.all())
     # issues = serializers.PrimaryKeyRelatedField(many=True, queryset=Issue.objects.all())
-    contributors = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    issues = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    contributors_id = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    issues_id = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Project
-        fields = ['title', 'description', 'type', 'contributors', 'issues']
+        fields = ['title', 'description', 'type', 'contributors_id', 'issues_id']
 
 
 class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ['description', 'author', 'issue', 'created_time']
+        fields = ['description', 'author_id', 'issue_id', 'created_time']
