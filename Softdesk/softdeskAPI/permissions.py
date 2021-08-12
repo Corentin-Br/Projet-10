c14b33cbfe09ef647c52cf306ac206a1e8f90b0e
Corentin BRAVO <corentinbravo@gmail.com>
2021-08-12 08:58:23 +0200

    Added a function to get a project from the view. Changed permissions from "has_object_permission" to "has_permission" where it's better.

--- Softdesk/softdeskAPI/permissions.py
+++ Softdesk/softdeskAPI/permissions.py
@@ -1,6 +1,14 @@
 from rest_framework import permissions
 
-from Softdesk.softdeskAPI.models import Contributor
+from .models import Contributor, Project
+
+
+def get_project(view):
+    if "project_pk" in view.kwargs:
+        project_pk = view.kwargs["project_pk"]
+    else:
+        project_pk = view.kwargs["pk"]
+    return Project.objects.get(pk=project_pk)
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
@@ -11,7 +19,7 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.author == request.user
+        return obj.author_id == request.user
 
 
 class HasCreatedProjectOrReadOnly(permissions.BasePermission):
@@ -19,18 +27,27 @@ class HasCreatedProjectOrReadOnly(permissions.BasePermission):
     Custom permission to only allow authors of a project to edit or delete it.
     """
 
-    def has_object_permission(self, request, view, project):
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        contributor = Contributor.objects.get(user=request.user, project=project)
-        # contributor = [contributor for contributor in request.user.contributors if contributor.project == project]
-        # return contributor[0].role if contributor else False
-        return contributor.role == "creator"
+        contributor = Contributor.objects.get(user_id=request.user, project_id=get_project(view))
+        return contributor.role == "author"
+
+
+# class IsProjectCreatorOrReadOnly(permissions.BasePermission):
+#     """
+#     Custom permission to only allow authors of a project to edit or delete it.
+#     """
+#
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in permissions.SAFE_METHODS:
+#             return True
+#         return obj.role == "author"
 
 
 class IsContributor(permissions.BasePermission):
     """
     Custom permission to only allow contributors of a project to see it.
     """
-    def has_object_permission(self, request, view, project):
-        return Contributor.objects.filter(user=request.user, project=project).exists()
+    def has_permission(self, request, view):
+        return Contributor.objects.filter(user_id=request.user, project_id=get_project(view)).exists()
