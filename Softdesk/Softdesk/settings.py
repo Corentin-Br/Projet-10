ea5611d6a9a44cdb9253cbd091fb211160be99f3
Corentin BRAVO <corentinbravo@gmail.com>
2021-08-12 18:16:21 +0200

    Added JWT authentication

--- Softdesk/Softdesk/settings.py
+++ Softdesk/Softdesk/settings.py
@@ -126,8 +126,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'softdeskAPI.MyUser'
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
